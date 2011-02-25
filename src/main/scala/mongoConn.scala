6e9c7f5a9bf18c267d29b2a3f266bf98ac703830
dontask <dontask@ecksbocks.(none)>
2011-02-25 00:31:45 -0800

    basic mongo/casbah project using the Scala Built Tool.

--- src/main/scala/mongoConn.scala
+++ src/main/scala/mongoConn.scala
@@ -1,21 +1,14 @@
 package main.scala
 
-/**
- * Created by IntelliJ IDEA.
- * User: dontask
- * Date: 2/12/11
- * Time: 10:51 AM
- * To change this template use File | Settings | File Templates.
- */
-
 import com.mongodb.casbah.Imports._
-import scala.xml._
 import scala._
 
-class mongoConn extends Application{
+object mongoConn extends Application
+{
+      var mongoColl = MongoConnection()("temp")("first")
 
-val mongoColl = MongoConnection()("temp")("first")
+      var scalaColl = mongoColl.find();
 
-println(mongoColl.find())
+      scalaColl.foreach( x => println(x("hobby")));
 
 }
